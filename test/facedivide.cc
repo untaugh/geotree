a27ad1e1de5aa2bdde50d085b4adccebc1350a5c
Oskar Rundgren <orundg@gmail.com>
2018-05-11 21:44:16 +0100

    Fix compiler warnings. Remove unused code.

--- test/facedivide.cc
+++ test/facedivide.cc
@@ -10,15 +10,6 @@ namespace {
 
   TEST_F(FaceDivideTest, Basic)
   {
-    // MeshFactory factory;
-    //
-    // Mesh tetra0 = factory.tetra(10);
-    // Mesh tetra1 = factory.tetra(10);
-    //
-    // tetra1.translate(Vector3d(1, 1, 1));
-    //
-    // IntersectionPoints points0(tetra0, tetra1);
-
     Vector2d p0(0,0);
     Vector2d p1(1,0);
     Vector2d p2(0,1);
@@ -40,11 +31,11 @@ namespace {
 
   TEST_F(FaceDivideTest, Edgepoint)
   {
-    edgepoint p0 = {.segment = 0, .index = 0, .distance = 0.0};
-    edgepoint p1 = {.segment = 0, .index = 0, .distance = 0.1};
-    edgepoint p2 = {.segment = 0, .index = 0, .distance = 0.2};
-    edgepoint p3 = {.segment = 1, .index = 0, .distance = 0.0};
-    edgepoint p4 = {.segment = 2, .index = 0, .distance = 1.0};
+    edgepoint p0 = {0, 0, 0.0};
+    edgepoint p1 = {0, 0, 0.1};
+    edgepoint p2 = {0, 0, 0.2};
+    edgepoint p3 = {1, 0, 0.0};
+    edgepoint p4 = {2, 0, 1.0};
 
     EXPECT_TRUE(p0 < p1);
     EXPECT_TRUE(p0 < p2);
